9027bdc8c33a128f0d7a41f67db146620d3fb643
alxww55 <alxww55@icloud.com>
2025-01-12 10:20:54 +0100

    Layout, Background ready to config Control Panel

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/style/main.css"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+  plugins: [],  
+  }
+}
